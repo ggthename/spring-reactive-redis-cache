8de17cfac8f10e11ca5b9c28c6e726cb10cdfa45
yoonwon.ko <1qazxsw@3e>
2022-07-21 19:04:37 +0900

    ttl 세팅을 위한 setter 추가

--- src/main/java/org/my/reactor/ReactiveRedisProperties.java
+++ src/main/java/org/my/reactor/ReactiveRedisProperties.java
@@ -1,10 +1,12 @@
 package org.my.reactor;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix="spring.cache.redis")
 @Getter
+@Setter
 public class ReactiveRedisProperties {
     private long defaultExpireSeconds = -1;
 }
